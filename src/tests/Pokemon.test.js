6a6c8775fad1fb8f8f37531067e19f8c2baf91a9
raphael <raphabarzotto@gmail.com>
2022-01-03 02:08:37 -0300

    entrega requisito 6

--- src/tests/Pokemon.test.js
+++ src/tests/Pokemon.test.js
@@ -1,1 +1,40 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from './renderWithRouter';
+
+describe('Testa o componente Pokemon', () => {
+  beforeEach(() => renderWithRouter(<App />));
+  // É renderizado um card com as informações de determinado pokémon
+  it('É renderizado um card com as informações de determinado pokémon', () => {
+    const pokemonName = screen.getByTestId('pokemon-name');
+    const pokemonType = screen.getByTestId('pokemon-type');
+    const pokemonWeight = screen.getByTestId('pokemon-weight');
+    const pokemonImage = screen.getByAltText('Pikachu sprite').src;
+    expect(pokemonName).toHaveTextContent('Pikachu');
+    expect(pokemonType).toHaveTextContent('Electric');
+    expect(pokemonWeight).toHaveTextContent('Average weight: 6.0 kg');
+    expect(pokemonImage).toBe('https://cdn2.bulbagarden.net/upload/b/b2/Spr_5b_025_m.png');
+  });
+
+  // Possui link de navegação para exibir detalhes deste Pokémon.
+  it('Possui link de navegação para exibir detalhes deste Pokémon.', () => {
+    const btnMoreDetails = screen.getByRole('link', { name: /more details/i });
+    expect(btnMoreDetails).toBeInTheDocument();
+    expect(btnMoreDetails.href).toBe('http://localhost/pokemons/25');
+  });
+
+  // Existe um ícone de estrela nos Pokémons favoritados
+  it('Existe um ícone de estrela nos Pokémons favoritados', () => {
+    const btnMoreDetails = screen.getByRole('link', { name: /more details/i });
+    userEvent.click(btnMoreDetails);
+
+    const checkboxFav = screen.getByRole('checkbox', { name: /pokémon favoritado\?/i });
+    userEvent.click(checkboxFav);
+
+    const starIcon = screen.getByRole('img', { name: /pikachu is marked as favorite/i });
+    expect(starIcon).toBeInTheDocument();
+    expect(starIcon.src).toBe('http://localhost/star-icon.svg');
+  });
+});
